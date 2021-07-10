7dfe9683a4d38b5274590bb79017eb6e29c130e4
hvhvdevdev <86674264+hvhvdevdev@users.noreply.github.com>
2021-07-10 22:00:22 +0700

    init

---
+++ hvLinkedList.h
@@ -0,0 +1,60 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef LIBHVCOLS_HVLINKEDLIST_H
+#define LIBHVCOLS_HVLINKEDLIST_H
+
+#ifndef Item
+#define Item int
+#endif
+
+#define _CONCAT(id1, id2) id1##id2
+#define CONCAT(id1, id2) _CONCAT(id1,id2)
+
+#define hvLinkedListOfItem CONCAT(hvLinkedListOf,Item)
+#define hvLinkedList_push_back_Item CONCAT(hvLinkedList_push_back_,Item)
+
+/** \brief Linked List Data Structure.
+ *
+ */
+typedef struct hvLinkedListOfItem {
+    Item item;
+    struct hvLinkedListOfItem *next;
+} hvLinkedListOfItem;
+
+bool hvLinkedList_push_back_Item(hvLinkedListOfItem **head, Item item) {
+    hvLinkedListOfItem *node = malloc(sizeof(hvLinkedListOfItem));
+    // Allocation failed?
+    if (!node) {
+        return false;
+    }
+
+    // Initialize node
+    node->next = NULL;
+    node->item = item;
+
+    hvLinkedListOfItem *ptr = *head;
+    // Is empty?
+    if (!ptr) {
+        *head = node;
+        return true;
+    }
+
+    while (ptr) {
+        // Insert node at back.
+        if (!ptr->next) {
+            ptr->next = node;
+            return true;
+        }
+        ptr = ptr->next;
+    }
+
+    // Should not happen
+    return false;
+}
+
+
+#undef hvLinkedListOfItem
+#undef hvLinkedList_push_back_Item
+#endif //LIBHVCOLS_HVLINKEDLIST_H
---
+++ tests.c
@@ -0,0 +1,48 @@
+#include <string.h>
+#include <stdio.h>
+#include "vendor/bdd-for-c.h"
+#include "hvLinkedList.h"
+
+#undef LIBHVCOLS_HVLINKEDLIST_H
+#define Item float
+
+#include "hvLinkedList.h"
+
+#undef Item
+
+spec("LinkedList append") {
+    static hvLinkedListOfint *my_list;
+    static hvLinkedListOffloat *my_float_list;
+
+    context("my_list is empty") {
+        before() {
+            my_list = NULL;
+        }
+
+        it("should append 1 successfully") check(hvLinkedList_push_back_int(&my_list, 1))
+        it("should have 1 as the first item") check(my_list->item == 1)
+        it("should not have second item") check(my_list->next == NULL)
+    }
+
+    context("my_list have 1 as its only item") {
+        before() {
+            my_list = NULL;
+            hvLinkedList_push_back_int(&my_list, 1);
+        }
+        it("should append 2 successfully") check(hvLinkedList_push_back_int(&my_list, 2))
+        it("should still have 1 as the first item") check(my_list->item == 1)
+        it("should have 2 as the second item") check(my_list->next->item == 2)
+        it("should not have third item") check(my_list->next->next == NULL)
+    }
+
+    context("my_float_list have 1.0f as its only item") {
+        before() {
+            my_float_list = NULL;
+            hvLinkedList_push_back_float(&my_float_list, 1.0f);
+        }
+        it("should append 2.5f successfully") check(hvLinkedList_push_back_float(&my_float_list, 2.5f))
+        it("should still have 1.0f as the first item") check(my_float_list->item == 1.0f)
+        it("should have 2.5f as the second item") check(my_float_list->next->item == 2.5f)
+        it("should not have third item") check(my_float_list->next->next == NULL)
+    }
+}
